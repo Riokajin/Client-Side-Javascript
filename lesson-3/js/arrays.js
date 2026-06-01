6c4f4c4481e568c0bbd005c6c1cbb92ab2236976
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-06-01 15:46:41 -0400

    class work

--- lesson-3/js/arrays.js
+++ lesson-3/js/arrays.js
@@ -2,36 +2,53 @@ const output = document.querySelector('body p:nth-of-type(2)');
 
 /* STEP 1: Creating an array
 When declaring and initializing an array, you can include strings, numbers, booleans, and even other arrays */
+// let myArray = new Array;
+let myArray = ["string", true, 100, [5, "hello"]];
 
 /* STEP 2: Reading and changing array elements
 You can refer to a particular element in an array with it's index number */
-
+myArray[1] = false;
+myArray[3][0] = 6;
+// output.textContent = `The 3rd element in the array is ${String(myArray[2])}, which is a ${typeof(myArray[2])}!`;
 // You can also change a particular element
+myArray[3][1] = true;
+console.log
 
 // An array within an array is called a multidimensional array - it can be accessed by specifying the index of the first array, then the item within it
 /* STEP 3: Determining array length
 Being able to figure out how many elements are contained in an array is a critical feature of JavaScript programming */
-
+output.textContent = `myArray has ${myArray.length} elements inside of it.`
+output.textContent = "";
 // In particular, looping through arrays
-
+for (let i = 0; i < myArray.length; i++) {
+    //check to see if element is an array
+    if (Array.isArray(myArray[i])) {
+        for (let j = 0; j < myArray[i].length; j++) {
+            output.textContent += `${myArray[i][j]}, `;
+        }
+    } else {
+        output.textContent += `${myArray[i]}, `;
+    }
+}
 /* STEP 4: Convert a string to an array
 If there is a common character that can act as a delimiter in a string, we can use this character to create an array */
-
+let orig6 = "Toronto Maple Leafs, Chicago Blackhawks, Detroit Red Wings, Boston Bruins, Montréal Canadiens, New York Rangers"
 // Output one of the array items
 
 // Output the last element of the array
 
 /* STEP 5: Convert an array back to a string
 Use join() and toString() - note that join() allows you to choose and insert a delimiter, while toString() does not */
+let orig6String = orig6Array.join(" / ");
 
 /* STEP 6: Adding and removing items from an array
 Without the ability to edit the contents of an array, this type of variable would have limited use - but adding and removing array items is pretty straightforward */
 
-// Adding one or more items to an array with push()
-
+// Adding one or more items to an array with push() similar to append
+orig6Array.push("Buffalo Sabres");
 // If you would like to capture how many elements are in the array after you have edited it, then…
 
-// Removing an item from an array with pop()
+// Removing an item from an array with pop() the opposite of append only the end.
 
 // pop() returns the item that was removed, rather than the length of the updated array, so…
 
--- lesson-4/js/conditionals.js
+++ lesson-4/js/conditionals.js
@@ -4,6 +4,16 @@ const output = document.querySelector('body p:nth-of-type(2)');
 let homeWorkDone = null;
 
 // STEP 1b: Any value that is not false, undefined, null, 0, NaN, or an empty string will evaluate to TRUE when tested using a conditional statement - try a few other values for var homeWorkDone and test
+let homeworkDone = null;
+let breakDuration;
+if (homeworkDone) {
+	breakDuration = 60;
+} else {
+	breakDuration = 10;
+}
+
+output.textContent = `I get ${breakDuration} minutes for a break!`;
+
 
 /* STEP 2: The more complex if|elseif|else */
 const page = document.querySelector('html');
@@ -14,34 +24,52 @@ const weatherButton = document.querySelector('#weatherButton');
 const comments = document.querySelector('#commentary');
 
 /* STEP 2a: Add an event listener for the 'click' event on the 'Set Weather' button that invokes the setWeather() function below */
-
+weatherButton.addEventListener("click", setWeather)
 
 function setWeather() {
+	// console.log("Event listener works!")
 	let choice = select.value;
 	let temperature = temp.value;
 	/* STEP 2b: Craft an IF/ELSEIF/ELSE that changes the src attribute of the icon element to the appropriate .svg file in the images folder */
+if (choice === "sunny") {
+	icon.src = "images/sunny.svg";
+	// change screen colour based on temperature
 
+} else if (choice === "rainy") {
+	icon.src = "images/rainy.svg";
+} else if (choice === "windy") {
+	icon.src = "images/windy.svg";
+} else {
+	icon.src = "images/cloud-off.svg";
+}
+setBackgroundColour(temperature);
 	/* STEP 3: Add a nested IF/ELSE statement inside the 'sunny' condition above that tests for temperature, and if it is equal to or greater than 15, turn the page background orange, otherwise turn it lightblue */
-
+function setBackgroundColour() {
+	if (temperature >= 15) {
+		page.style.backgroundColor = "orange";
+	} else {
+		page.style.backgroundColor = "lightblue";
+	}
+}
 	/* STEP 4: Logical operators - and, or, not (delete multi-line comment delimiters below) */
 	// STEP 4a: AND - && (It's sunny AND it's at least 15deg)
-/*
-	if () {
+
+	if (choice === "sunny" && temperature >= 15) {
 		comments.textContent = 'It is sunny and warm — time to find your sunglasses!';
 	// STEP 4b: AND, once again - && (It's sunny AND it's less than 15deg)
-	} else if () {
+	} else if (choice === "sunny" && temperature < 15) {
 		comments.textContent = 'It is sunny out there, but it is a bit cool.';
 	// STEP 4c: OR - || (It's windy OR rainy)
-	} else if () {
+	} else if (choice === "windy" || "rainy") {
 		comments.textContent = 'The weather today is a bit unsettled.';
 	} else {
 		comments.textContent = 'You did not specify the type of weather, today.';
 	}
 	// STEP 4d: NOT - ! (It's NOT rainy)
-	if () {
+	if (choice !== "rainy") {
 		comments.textContent += ' No sign of rain.';
 	}
-*/
+
 	// Weather icons by Cole Bemis - https://feathericons.com/, MIT, https://commons.wikimedia.org/w/index.php?curid=60153354
 
 } // End of setWeather() function
@@ -57,21 +85,30 @@ windSpeed.addEventListener('change', getWindSpeed);
 function getWindSpeed() {
 let speed = Number(windSpeed.value);
 // STEP 5a: Build the SWITCH code block
-
+switch (speed) {
 	// STEP 5b: Build out four cases, followed by a default
-
+	case 100:
 		// 100 km/h
 		windComment.textContent = speed + 'km/h - Holy schmoly! Hold on to your hat!';
-
+		break;
+	case 75:
 		// 75 km/h
 		windComment.textContent = speed + 'km/h - It\'s a-howling out there!';
-
+		break;
+	case 50:
 		// 50 km/h
 		windComment.textContent = speed + 'km/h - Quite gusty out there, today.';
-
+		break;
+	case 25:
 		// 25 km/h
 		windComment.textContent = speed + 'km/h - A wee bit breezy.';
-
+		break;
+	case 0:
+		windComment.textContent = speed + 'km/h - Still winds today.';
+		break;
+	default:
+		windComment.textContent = "No wind speed available";
+	}
 } // End of getWindSpeed() function
 
 
@@ -79,6 +116,6 @@ let speed = Number(windSpeed.value);
 This piece of syntax is a bit less typing, but it is harder to read */
 let breakMessage = document.querySelector('#breakTime');
 let coffeeBreak = true;
-
+coffeeBreak === true ? breakMessage.textContent = "Coffee time!" : breakMessage.textContent = "Keep working.";
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals
