1beaa4641bbccbd82bf402da2c1f993dfdab8d42
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-06-01 17:44:42 -0400

    dom selectors

--- assignment-1/js/script.js
+++ assignment-1/js/script.js
@@ -1,3 +1,4 @@
+// Arrays for each part of the story
 let characters = [
     "Cecil", 
     "Bartz", 
@@ -43,3 +44,26 @@ let endings = [
 
 storyOutput.textContent = 
     `${characters[charIndex]} ${actions[actionIndex]} ${things[thingIndex]} ${locations[locationIndex]} ${endings[endingIndex]}`;
+
+// Index counters for cycling through each array
+let charIndex = 0;
+let actionIndex = 0;
+let thingIndex = 0;
+let locationIndex = 0;
+let endingIndex = 0;
+
+// Grab all the buttons and output spans from the page
+const charBtn = document.querySelector("#charBtn");
+const actionBtn = document.querySelector("#actionBtn");
+const thingBtn = document.querySelector("#thingBtn");
+const locationBtn = document.querySelector("#locationBtn");
+const endingBtn = document.querySelector("#endingBtn");
+
+const charOut = document.querySelector("#charOut");
+const actionOut = document.querySelector("#actionOut");
+const thingOut = document.querySelector("#thingOut");
+const locationOut = document.querySelector("#locationOut");
+const engingOut = document.querySelector("#endingOut");
+
+const showStoryBtn = document.querySelector("#showStoryBtn");
+const storyOutput = document.querySelector("#storyOutput");
